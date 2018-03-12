5081442cef1f426de3002bd3dba76cc321b9c2b1
Geoffrey <migault@hotmail.fr>
2018-03-12 15:44:53 +0100

    Change path to easyRLS

--- Matlab/Utils/script.m
+++ Matlab/Utils/script.m
@@ -5,12 +5,12 @@ clear
 clc
 
 %% go in the project directory
-cd /home/ljp/Science/Projects/RLS_Hugo/
+cd /home/ljp/Science/Projects/easyRLS/
 param.cwd = pwd;
 %% get focus
-param.date = '2018-01-11';
-param.run_number = 5;
-param.Layers = 3:12; 
+param.date = '2018-01-31';
+param.run_number = 1;
+param.Layers = 3:20; 
 F = NT.Focus({param.cwd, '', param.date, param.run_number});
 %% create binary file from Tif
 tifToMmap(F, {'z', param.Layers});
@@ -52,8 +52,8 @@ maskViewer(F)
 
 
 %% load library to compute baseline
-[~,~] = loadlibrary('/home/ljp/Science/Projects/RLS_Hugo/Programs/NeuroTools/Tools/caTools.so',...
-                    '/home/ljp/Science/Projects/RLS_Hugo/Programs/NeuroTools/Tools/caTools.h');
+[~,~] = loadlibrary('/home/ljp/Science/Projects/easyRLS/Programs/easyRLS/Tools/caTools/caTools.so',...
+                    '/home/ljp/Science/Projects/easyRLS/Programs/easyRLS/Tools/caTools/caTools.h');
 %% compute baseline using caTools library
 caToolsRunquantile(F, param.Layers);
 %% view baseline
