77d0386d06f4a6a3d30a3ff9fe2f133420f0f2a2
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-03-27 15:00:48 +0200

    minor changes

---
+++ Matlab/RAS/transposeStack.m
@@ -0,0 +1,20 @@
+function stackOut = transposeStack(stackIn, invertXY, invertX, invertY, invertZ)
+%transposeStack is like transposeImage, but more for dimension 4
+% TODO write it properly
+stackOut = stackIn;
+
+% transpose 
+    if invertXY
+        stackOut = permute(stackOut, [2 1 3 4]);
+    end
+    if invertX
+        stackOut = flip(stackOut, 1);
+    end
+    if invertY
+        stackOut = flip(stackOut, 2);
+    end
+    if invertZ
+        stackOut = flip(stackOut, 3);
+    end
+    
+end
