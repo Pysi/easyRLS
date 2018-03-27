8e30ea5b056517b58412f2f9ef62aeaf166851b3
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-03-27 16:17:37 +0200

    stackviewer tries to find rawRAS or dcimg when asked for mask

--- Matlab/+Focused/stackViewer.m
+++ Matlab/+Focused/stackViewer.m
@@ -1,16 +1,33 @@
 function stackViewer(F, tag)
-% focused version of stackviewer
+%Focused.stackViewer is the focused wrapper for stackViewer
+% it creates the memory map and optionally loads the mask
 
     titleFig = F.name; % get title
+    mask = []; % default mask
+    sp = split(tag, '.'); % splits the tag to see if there is an extension
 
     if strcmp(tag, 'ROImask') % particular case to view mask contour
         load(fullfile(F.dir.IP, 'mask.mat'), 'mask');
-        m = Focused.MmapOnDCIMG(F, F.run, {}); % get memory map
-    else
-        mask = [];
-        m = Focused.Mmap(F, tag); % get memory map
+        try
+            m = Focused.Mmap(F, 'rawRAS');
+        catch
+            try
+                m = Focused.MmapOnDCIMG(F, F.run, {});
+            catch
+                error('could not find rawRAS nor dcimg');
+            end
+        end
+        
+    elseif strcmp(sp{end}, 'dcimg') % if extension is dcimg
+        m = Focused.MmapOnDCIMG(F, sp{1}, {}); % get memory map on dcimg
+        titleFig = [titleFig ' (dcimg)'];
+        
+    else % normal case
+        m = Focused.Mmap(F, tag);
+        
     end
 
+    % call stackViewer
     stackViewer(m, titleFig, mask)
 
 end
