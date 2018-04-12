008a065cdfce23a6a6d5ed168107a8c065587f18
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-04-12 17:21:56 +0200

    compute drift on external stack possible

--- Matlab/Compute/driftCompute.m
+++ Matlab/Compute/driftCompute.m
@@ -13,17 +13,25 @@ function driftCompute(F, kwargs)
 
 % create wrapper object
 m = Focused.Mmap(F, 'rawRAS');
+mRef = m;
 
 % parse input to change reference stack TODO write validation function
 in = inputParser;
 in.addParameter('RefIndex', 10);        % by default 10th stack
+in.addParameter('RefStack', '');        % by default none
 in.addParameter('RefLayers', 8:10);     % by default stacks 8, 9, 10
-in.addParameter('Layers', [F.sets.id]); % by default all
+% in.addParameter('Layers', [F.sets.id]); % by default all 1 2 3 ... 20
 in.parse(kwargs{:})
 
 % Layers = in.Results.Layers;
+RefStack = in.Results.RefStack;
 RefIndex = in.Results.RefIndex;
 RefLayers = in.Results.RefLayers;
+
+if RefStack % if we want to use a reference stack which is outside the stack
+    mRef = Focused.Mmap(F, RefStack);
+    RefIndex = 1;
+end
 
 % --- Define reference image ---
 % the reference image we take is the maximum of the selected layers along
@@ -33,13 +41,13 @@ bbox = [ 53 566 45 914 ]; % define bounding box to look into
 X = bbox(1):bbox(2);
 Y = bbox(3):bbox(4);
 
-% compute reference image with max
-Ref = NT.Image(max(m(X,Y, RefLayers, RefIndex),[],3));
+% compute reference image with max and loads it in Raphael's image object
+Ref = NT.Image(max(mRef(X,Y, RefLayers, RefIndex),[],3));
 
 % --- Drift correction ---
 % creates a figure to plot the drift correction
 figure; hold on;
-title([F.name '   dx=red, dy=blue']);
+title([F.name '   dx=red, dy=green']);
 
 % init drift vectors
 dx = zeros(1,m.t);
@@ -54,8 +62,8 @@ for t = m.T % run across the times
     
     % plot 1/50 figures
     if ~mod(t,50)
-        plot(t-49:t,dy(t-49:t),'b.');
         plot(t-49:t,dx(t-49:t),'r.');
+        plot(t-49:t,dy(t-49:t),'g.');
         pause(0.01);
     end   
 end
