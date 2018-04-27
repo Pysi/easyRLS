de01064a6f6235bf3524b31efdd77718ff816e93
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-04-27 15:18:37 +0200

    updated default config

--- Matlab/Utils/DEFAULT_CONFIG.m
+++ Matlab/Utils/DEFAULT_CONFIG.m
@@ -1,6 +1,6 @@
 % DEFAULT CONFIG : do not change
 
-% if you want to set your config copy it in an other file (ex CONFIG.m)
+% if you want to set your config copy it in an other file (ex CONFIG_HUGO.m)
 % and ignore it in the git
 
 path.caTools = '/home/ljp/Science/Programs/easyRLS/Tools/caTools/';
@@ -8,9 +8,11 @@ path.program = '/home/ljp/Science/Programs/';
 
 path.root = '/home/ljp/Science/Projects/RLS/';
 param.study = '';
-param.date = '2018-01-31';
-param.run = 8;
+param.date = '0000-00-00';
+param.run = 0;
 
 param.Layers = 3:20; 
 param.RefLayers = 8:10;
 param.RefIndex = 10; 
+
+% you can add your own functions in the CONFIG_HUGO.m 
