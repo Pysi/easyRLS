de7d36c43d849d3e69a3259e59c6f2ff654436a0
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-05-29 14:22:35 +0200

    minor changes

--- Matlab/CONFIG_Hugo.m
+++ Matlab/CONFIG_Hugo.m
@@ -2,22 +2,23 @@
 
 clear; clc
 
-cd /home/ljp/Programs/
-addpath(genpath('easyRLS/Matlab'))
-addpath(genpath('NeuroTools/Matlab'))
-
 root = '/home/ljp/Science/Projects/RLS1P/';
 study = '';
 date = '2018-05-21';
 run = 3;
 
+cd(root)
+addpath(genpath('easyRLS/Matlab'))
+addpath(genpath('NeuroTools/Matlab'))
+
 F = NT.Focus(root, study, date, run);
 
-F.Analysis.Layers = 3:20;
-F.Analysis.RefLayers = 6:6;
-F.Analysis.RefIndex = 10;
-F.Analysis.RefStack = '';
+Analysis.Layers = 3:20;
+Analysis.RefLayers = 6:6;
+Analysis.RefIndex = 10;
+Analysis.RefStack = '';
 
+F.Analysis = Analysis;
 
 %% load library to compute baseline
 
@@ -27,18 +28,51 @@ cd(F.dir('caTools'))
 
 %% workflow
 
-Focused.driftCompute(F, 'dcimg.dcimg');
-driftApply(F, 'dcimg.dcimg');
-computeBackground(F, 'corrected');
-createGrayStack(F)
-segmentBrain(F, 'graystack');
-computeBaselineNeuron(F, 50);
-computeBaselinePixel(F, 20:3, 50);
-dffNeuron(F);
-dffPixel(F, 20:3);
-phaseMapPixel(F, 0.2)
+RUNS = [4 6 8];
 
+%%% manual part
+for run = RUNS
+    F = NT.Focus(root, study, date, run);
+    F.Analysis = Analysis;
+    semiAutoROI(F, 'dcimg.dcimg');
+end
 
+%%% automatic part
+for run = RUNS
+    % init focus
+    F = NT.Focus(root, study, date, run);
+    F.Analysis = Analysis;
+    
+    % do stuff
+    Focused.driftCompute(F, 'dcimg.dcimg');
+    driftApply(F, 'dcimg.dcimg');
+    computeBackground(F, 'corrected');
+    createGrayStack(F)
+    segmentBrain(F, 'graystack');
+    computeBaselineNeuron(F, 50);
+    computeBaselinePixel(F, 3:20, 50);
+    dffNeuron(F);
+    dffPixel(F, 3:20);
+    phaseMapPixel(F, 0.2)
+    phaseMapNeuron(F, 0.2)
+end
+
+F = NT.Focus(root, study, date, 4);
+F.Analysis = Analysis;
+
+function workflow(F)
+    Focused.driftCompute(F, 'dcimg.dcimg');
+    driftApply(F, 'dcimg.dcimg');
+    computeBackground(F, 'corrected');
+    createGrayStack(F)
+    segmentBrain(F, 'graystack');
+    computeBaselineNeuron(F, 50);
+%     computeBaselinePixel(F, 3:20, 50);
+    dffNeuron(F);
+%     dffPixel(F, 3:20);
+%     phaseMapPixel(F, 0.2)
+    phaseMapNeuron(F, 0.2)
+end
 
 %%
 path.RefBrains = '/home/ljp/Science/RefBrains';
--- Matlab/main.m
+++ Matlab/main.m
@@ -1,46 +1,50 @@
 % this script helps to understand easyRLS step by step
 % Hugo Trentesaux 2018-05-23
 
+%%% This file is a collection of existing function. You can paste them in
+%%% your workflow
+
 %% view dcimg / tif
 Focused.stackViewer(F, 'dcimg.dcimg'); % TODO define default name for dcimg
 Focused.stackViewer(F, 'images.tif');
-%% semi auto ROI on dcimg / tif
+%% --- %% semi auto ROI on dcimg / tif %% --- %%
 semiAutoROI(F, 'dcimg.dcimg'); % let you adjust automatic ROI
 semiAutoROI(F, 'images.tif'); % let you adjust automatic ROI
 %% check if ROI is ok
 Focused.stackViewer(F, 'ROImask'); % stack viewer behaves differently for argument 'ROImask'
-%% drift compute
+%% --- %% drift compute %% --- %%
 Focused.driftCompute(F, 'dcimg.dcimg');
 Focused.driftCompute(F, 'images.tif');
 %% see drift correction before applying
 seeDriftCorrection(F, 'dcimg.dcimg');
 seeDriftCorrection(F, 'images.tif');
-%% apply drift if satisfacted
+%% --- %% apply drift if satisfacted %% --- %%
 driftApply(F, 'dcimg.dcimg');
 driftApply(F, 'images.tif');
 %% view corrected stack
 Focused.stackViewer(F, 'corrected');
-%% compute background
+%% --- %% compute background %% --- %%
 computeBackground(F, 'corrected');
-%% compute gray stack
+%% --- %% compute gray stack
 createGrayStack(F)
 %% view gray stack
 Focused.stackViewer(F, 'graystack')
-%% segment neurons
+%% --- %% segment neurons %% --- %%
 segmentBrain(F, 'graystack');
-%% compute baseline per neuron / pixel
+%% --- %% compute baseline per neuron / pixel %% --- %%
 computeBaselineNeuron(F, 50);
-computeBaselinePixel(F, 4:10, 50);
-%% diplay it
+computeBaselinePixel(F, 3:20, 50);
+%% display it
 stackViewer2D(F, 'BaselineNeuron');
 stackViewer2D(F, 'BaselinePixel');
-%% compute dff per neuron / pixel
+%% --- %%% compute dff per neuron / pixel %% --- %%
 dffNeuron(F);
-dffPixel(F, 4:10);
+dffPixel(F, 3:20);
 %% display it
 stackViewer2D(F, 'DFFNeuron');
 stackViewer2D(F, 'DFFPixel');
-%% compute phase map neuron / pixel
+%% --- %% compute phase map neuron / pixel %% --- %%
+phaseMapNeuron(F, 0.2)
 phaseMapPixel(F, 0.2)
 %% display it
 Focused.phaseMapViewer(F, 'neuron')
