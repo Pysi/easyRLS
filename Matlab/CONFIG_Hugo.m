64781bfa41852629e6f26038d877cb1e47508c38
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-05-30 10:52:14 +0200

    automated workflow

--- Matlab/CONFIG_Hugo.m
+++ Matlab/CONFIG_Hugo.m
@@ -17,7 +17,8 @@ Analysis.Layers = 3:20;
 Analysis.RefLayers = 6:6;
 Analysis.RefIndex = 10;
 Analysis.RefStack = '';
-
+% TODO add refbrain here and to focus
+Analysis.Lineage = 'nuclear';
 F.Analysis = Analysis;
 
 %% load library to compute baseline
@@ -28,51 +29,70 @@ cd(F.dir('caTools'))
 
 %% workflow
 
-RUNS = [4 6 8];
-
-%%% manual part
-for run = RUNS
-    F = NT.Focus(root, study, date, run);
-    F.Analysis = Analysis;
-    semiAutoROI(F, 'dcimg.dcimg');
-end
-
-%%% automatic part
-for run = RUNS
-    % init focus
-    F = NT.Focus(root, study, date, run);
-    F.Analysis = Analysis;
-    
-    % do stuff
-    Focused.driftCompute(F, 'dcimg.dcimg');
-    driftApply(F, 'dcimg.dcimg');
-    computeBackground(F, 'corrected');
-    createGrayStack(F)
-    segmentBrain(F, 'graystack');
-    computeBaselineNeuron(F, 50);
-    computeBaselinePixel(F, 3:20, 50);
-    dffNeuron(F);
-    dffPixel(F, 3:20);
-    phaseMapPixel(F, 0.2)
-    phaseMapNeuron(F, 0.2)
-end
-
-F = NT.Focus(root, study, date, 4);
-F.Analysis = Analysis;
-
-function workflow(F)
-    Focused.driftCompute(F, 'dcimg.dcimg');
-    driftApply(F, 'dcimg.dcimg');
-    computeBackground(F, 'corrected');
-    createGrayStack(F)
-    segmentBrain(F, 'graystack');
-    computeBaselineNeuron(F, 50);
-%     computeBaselinePixel(F, 3:20, 50);
-    dffNeuron(F);
-%     dffPixel(F, 3:20);
-%     phaseMapPixel(F, 0.2)
-    phaseMapNeuron(F, 0.2)
-end
+date = '2018-05-21';
+RUNS = [ 3 4 6 8 ];
+prepare(root, study, date, Analysis, RUNS)
+analyse(root, study, date, Analysis, RUNS, @workflowPixel)
 
 %%
-path.RefBrains = '/home/ljp/Science/RefBrains';
+F = NT.Focus(root, study, '2018-05-25', 12);
+F.Analysis = Analysis;
+stackViewer2D(F, 'DFFNeuron');
+%%
+Focused.phaseMapViewer(F, 'neuron')
+
+%%
+date = '2018-05-25';
+
+% RUNS = [ 3 4 7 8 11 12 15 ]; 
+% prepare(root, study, date, Analysis, RUNS)
+
+TTT = tic;
+
+RUNS = [ 3 4 7 8 ]; 
+Analysis.Lineage = 'cytoplasmic';
+analyse(root, study, date, Analysis, RUNS, @workflowNeuron)
+
+RUNS = [11 12 15];
+Analysis.Lineage = 'nuclear';
+analyse(root, study, date, Analysis, RUNS, @workflowNeuron)
+
+TIME = toc(TTT);
+disp(TIME);
+
+function prepare(root, study, date, Analysis, RUNS) % manual part
+    for run = RUNS
+        F = NT.Focus(root, study, date, run);
+        F.Analysis = Analysis;
+        semiAutoROI(F, 'dcimg.dcimg'); 
+    end
+end
+
+function analyse(root, study, date, Analysis, RUNS, workflow) % automatic part
+    for run = RUNS
+        try
+            F = NT.Focus(root, study, date, run);
+            F.Analysis = Analysis;
+            workflow(F)
+        catch me
+            warning(me.identifier, '%s\nworking on next one', me.message);
+        end
+    end
+end
+
+function workflowNeuron(F)
+    Focused.driftCompute(F, 'dcimg.dcimg');
+    driftApply(F, 'dcimg.dcimg');
+    computeBackground(F, 'corrected');
+    createGrayStack(F)
+    segmentBrain(F, 'graystack');
+    computeBaselineNeuron(F, 50);
+    dffNeuron(F);
+    phaseMapNeuron(F, 0.2)
+end
+
+function workflowPixel(F) % workflowNeuron must have been runned first
+    computeBaselinePixel(F, 3:20, 50);
+    dffPixel(F, 3:20);
+    phaseMapPixel(F, 0.2)
+end
