c465bbf469297a21b80d5757d797f9005ac68d36
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-06-08 16:25:29 +0200

    un-indented phasemappixel

--- Matlab/Compute/phaseMapPixel.m
+++ Matlab/Compute/phaseMapPixel.m
@@ -26,7 +26,7 @@ function phaseMapPixel(F)
     % create phasemap folder
     Focused.mkdir(F, 'PhaseMapPixel');
 
-    % get path to record data
+    % get path to record data (defines what should be output)
     labels = {'pmp_amplitude', 'pmp_phase', 'pmp_deltaphi', 'pmp_realpart', 'pmp_imaginary'};
     out = struct();
     outInfo = struct();
@@ -37,22 +37,22 @@ function phaseMapPixel(F)
     end
     
     % Define stimulation parameters
-        fstim = fstim;                              % Stimulation frequency
-        N = F.param.NCycles ;                       % Number of images per layer
-        fs = 1000 / (F.dt * F.param.NLayers);       % Frame rate at which images per layer are acquired
-        dt = 1/fs;                                  % Sampling period
-        T = N*dt;                                   % Total time of acquisition
+    fstim = fstim;                              % Stimulation frequency
+    N = F.param.NCycles ;                       % Number of images per layer
+    fs = 1000 / (F.dt * F.param.NLayers);       % Frame rate at which images per layer are acquired
+    dt = 1/fs;                                  % Sampling period
+    T = N*dt;                                   % Total time of acquisition
 
-        % Define frequency vector
-        f = fs*[0:1:N/2]/N;
+    % Define frequency vector
+    f = fs*[0:1:N/2]/N;
 
-        % Finds the peak at the given frequency
-        f_round = round(f,3);
-        ind_fstim = find(f_round==fstim);
+    % Finds the peak at the given frequency
+    f_round = round(f,3);
+    ind_fstim = find(f_round==fstim);
 
-        % Phase shift
-        phi_GCaMP = 0;%-0.6916;
-        % Phase shift because of the response of the GCaMP, get with the convolution of the stimulus with a Kernel
+    % Phase shift
+    phi_GCaMP = 0;%-0.6916;
+    % Phase shift because of the response of the GCaMP, get with the convolution of the stimulus with a Kernel
 
 % % % % % % LOOP % % % % % 
     % run across the layers
@@ -71,47 +71,36 @@ function phaseMapPixel(F)
         load(dffLayer, 'mmap', 'x', 'y', 'z', 't', 'Z', 'T', 'indices', 'numIndex');
         mdff = recreateMmap(F,mmap);
 
+        % creates a buffer to write phasemap for each label
+       	for label = labels
+            BUFFER.(label{:}) = zeros(x,y); % a buffer for the layer
+        end
+
         % run across all image pixels
-        index = 0; % number of index in dff
-        zerosToWrite = 0;
-        for i = 1:x*y
+        for i = indices 
+            % Calculate fourier transformation
+            Y = fft(mdff.Data.bit(:,index));
 
-            %if index is not in ROI (could check directly with mask, but mask could have changed)
-            if ~max(ismember(indices,i))
-                zerosToWrite = zerosToWrite +1;
-            %else index is in ROI and DFF is defined
-            else
-                index = index + 1; % increment by one
-                % write all zeros at the same time
-                for label = labels
-                    fwrite(out.(label{:}), zeros(1,zerosToWrite), 'single');
-                end
-                zerosToWrite = 0;
+            % extract peak from dff
+            pmp_amplitude = abs(Y(ind_fstim,:));
+            pmp_phase     = angle(Y(ind_fstim,:));
+            pmp_realpart  = real(Y(ind_fstim,:));
+            pmp_imaginary = imag(Y(ind_fstim,:));
+            pmp_deltaphi  = (pmp_phase - phase_delay + pi);
+                % -phase_delay = Shift positive of the fluorescence
+                % +pi = because of the fourier transform is done against a cosinus
                 
-                % Calculate fourier transformation
-                Y = fft(mdff.Data.bit(:,index));
-
-                % extract peak from dff
-                pmp_amplitude = abs(Y(ind_fstim,:));
-                pmp_phase     = angle(Y(ind_fstim,:));
-                pmp_realpart  = real(Y(ind_fstim,:));
-                pmp_imaginary = imag(Y(ind_fstim,:));
-                pmp_deltaphi = (pmp_phase - phase_delay + pi);
-                    % -phase_delay = Shift positive of the fluorescence
-                    % +pi = because of the fourier transform is done against a cosinus  
-
-                % write pixel in all binaries
-                for label = labels
-                    fwrite(out.(label{:}), eval(label{:}), 'single');
-                end
-
+            % fills buffer
+            for label = labels
+                BUFFER.(label{:})(i) = eval(label{:}); % a buffer for the layer
             end
-
         end
-        % write remaining zeros
-        for label = labels
-            fwrite(out.(label{:}), zeros(1,zerosToWrite), 'single');
-        end
+        
+        % write buffers in binary files
+        for label = labels 
+            fwrite(out.(label{:}), eval(label{:}), 'single'); 
+        end 
+        
         toc;
         
     end 
