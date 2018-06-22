f070ed82fb4bd64f65d32075cb8e1790263e5b5d
Geoffrey <migault@hotmail.fr>
2018-06-22 15:07:56 +0200

    Nuclear SemiAutoROI

--- Matlab/Utils/semiAutoROI.m
+++ Matlab/Utils/semiAutoROI.m
@@ -5,104 +5,123 @@ function semiAutoROI(F)
 %     - the tag of the binary you want to work on ('corrected' for instance)
 %     - the name of a dcimg if you want to work on a dcimg (Run00.dcimg for instance)
 
-    % set local parameters
-    minmax = [400 1200]; % minmax values for display
-    discretize = 50; % lower values means more point on contour
-    
-    % get global parameters
-    Z = F.Analysis.Layers;
-    t = F.Analysis.RefIndex;
-    
+% set local parameters
+minmax = [400 1200]; % minmax values for display
+discretize = 50; % lower values means more point on contour
+
+% get global parameters
+Z = F.Analysis.Layers;
+t = F.Analysis.RefIndex;
+
 % if the tag has an extension and that this extension is 'dcimg'
 % semiAutoROI will work on dcimg (particular case)
 
-    m = adapted4DMatrix(F,'source');
+m = adapted4DMatrix(F,'source');
 
 % % % % % % % % % % % % % % % % % % % % % % % % load existing or initialize
-    try % try to load existing mask.mat
-        disp('found mask file, loading it')
-        load(F.tag('mask'), 'mask'); % get the mask
-    catch % if no mask variable to load, initialize to NaN
-        Focused.mkdir(F, 'Mask');
-        disp('creating new mask file')
-        mask = false(m.x,m.y,20); % default total number of layers
-    end
+try % try to load existing mask.mat
+    disp('found mask file, loading it')
+    load(F.tag('mask'), 'mask'); % get the mask
+catch % if no mask variable to load, initialize to NaN
+    Focused.mkdir(F, 'Mask');
+    disp('creating new mask file')
+    mask = false(m.x,m.y,20); % default total number of layers
+end
 
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % print at screen
-    for z = Z % for each layer of concern
-        img = m(:,:,z,t); % load image
-        tmp2 = mask(:,:,z); % load mask layer
-
-        % show layer
-        hold off; f = imshow(img, minmax);
-
-        if ~max(max( tmp2 )) % if mask layer is null
-            % autocompute mask (TODO add slider)
-
-            % Auto brain countour (Geoffrey)
-% % <<<<<<< HEAD
-%             bg_img = mean2(img);
-%             H = fspecial('disk',20);
-%             tmp1 = imfilter(img,H,'replicate');
-%             tmp2 = img*0;
-%             tmp2(tmp1>bg_img*(1-(0.005*z))) = 1;
-%             tmp2 = bwareaopen(tmp2,(size(img,1)*size(img,2))/4);
-%             count = 4;
-%             while mean2(tmp2) == 0
-%                 count = count+1;
-%                 tmp2(tmp1>bg_img*(1-(0.005*z))) = 1;
-%                 tmp2 = bwareaopen(tmp2,round((size(img,1)*size(img,2))/count));
-%             end
-%=======
-            switch F.Analysis.Lineage 
-                case 'cytoplasmic'
-                    bg_img = mean2(img);
-                    H = fspecial('disk',20);
-                    tmp1 = imfilter(img,H,'replicate');
-                    tmp2 = img*0;
+for z = Z % for each layer of concern
+    img = m(:,:,z,t); % load image
+    tmp2 = mask(:,:,z); % load mask layer
+    
+    % show layer
+    hold off; f = imshow(img, minmax);
+    
+    if ~max(max( tmp2 )) % if mask layer is null
+        % autocompute mask (TODO add slider)
+        
+        % Auto brain countour (Geoffrey)
+        % % <<<<<<< HEAD
+        %             bg_img = mean2(img);
+        %             H = fspecial('disk',20);
+        %             tmp1 = imfilter(img,H,'replicate');
+        %             tmp2 = img*0;
+        %             tmp2(tmp1>bg_img*(1-(0.005*z))) = 1;
+        %             tmp2 = bwareaopen(tmp2,(size(img,1)*size(img,2))/4);
+        %             count = 4;
+        %             while mean2(tmp2) == 0
+        %                 count = count+1;
+        %                 tmp2(tmp1>bg_img*(1-(0.005*z))) = 1;
+        %                 tmp2 = bwareaopen(tmp2,round((size(img,1)*size(img,2))/count));
+        %             end
+        %=======
+        switch F.Analysis.Lineage
+            case 'cytoplasmic'
+                display('Cytoplasmic')
+                bg_img = mean2(img);
+                H = fspecial('disk',20);
+                tmp1 = imfilter(img,H,'replicate');
+                tmp2 = img*0;
+                tmp2(tmp1>bg_img*(1-(0.005*z))) = 1;
+                tmp2 = bwareaopen(tmp2,(size(img,1)*size(img,2))/4);
+                count = 4;
+                while mean2(tmp2) == 0
+                    count = count+1;
                     tmp2(tmp1>bg_img*(1-(0.005*z))) = 1;
-                    tmp2 = bwareaopen(tmp2,(size(img,1)*size(img,2))/4);
-                    count = 4;
-                    while mean2(tmp2) == 0
-                        count = count+1;
-                        tmp2(tmp1>bg_img*(1-(0.005*z))) = 1;
-                        tmp2 = bwareaopen(tmp2,round((size(img,1)*size(img,2))/count));
-                    end
-                otherwise
-                    tmp0 = img;
-                    tmp0(rangefilt(img) < mean2(rangefilt(img)/((Z(end)+1)/(Z(end) - z + 1)))) = 0;
-                    [B, L] = bwboundaries(tmp0);
-                    tmp0(L == 0) = 0;
-                    bg_img = mean2(img);
-                    H = fspecial('disk',1);
-                    tmp0 = imfilter(tmp0,H,'replicate');
-                    tmp2 = bwareaopen(tmp0,(size(img,1)*size(img,2))/4);
-            end
-%                 
-
-%>>>>>>> Geoffrey
-    %         sliderize(z, img, tmp2); % TODO make this work
+                    tmp2 = bwareaopen(tmp2,round((size(img,1)*size(img,2))/count));
+                end
+            otherwise
+                display('Nuclear')
+                tmp0 = img;
+                H = fspecial('disk',100);
+                tmp0 = imfilter(tmp0,H,'replicate');
+                tmp0(rangefilt(img) < 50) = 0;%((Z(end)+1)/(Z(end) - z + 1)) mean2(rangefilt(img)/1.5)
+                
+                [B, L] = bwboundaries(tmp0);
+                tmp0(L == 0) = 0;
+                tmp2 = bwareaopen(tmp0,(size(img,1)*size(img,2))/4);
+                count = 4;
+                while mean2(tmp2) == 0
+                    count = count+1;
+                    tmp2(tmp0>mean2(img)*(1-(0.005*z))) = 1;
+                    tmp2 = bwareaopen(tmp2,round((size(img,1)*size(img,2))/count));
+                end
+                H = fspecial('disk',30);
+                tmp2 = imfilter(tmp2,H,'replicate');
+                tmp2 = bwareaopen(tmp2,(size(img,1)*size(img,2))/4);
+                count = 4;
+                while mean2(tmp2) == 0
+                    count = count+1;
+                    tmp2(tmp0>mean2(img)*(1-(0.005*z))) = 1;
+                    tmp2 = bwareaopen(tmp2,round((size(img,1)*size(img,2))/count));
+                end
+                % imshow(tmp2);
+%                 imshow(tmp0, minmax);
         end
-
-        % plot contour over this
-        hold on; contour(tmp2);
-
-        % get new contour
-        tmp2 = editContour(f, tmp2, discretize);
-        if strcmp(tmp2, 'exit') % if exit
-            fprintf('layer %d was not saved\n', z);
-            return
-        end
-
-        % stores it in 'mask' variable and saves it
-        mask(:,:,z) = tmp2;
-        save(F.tag('mask'), 'mask'); % overwrites the mask
-        fprintf('saved layer %d\n', z);
-
+        %
+        
+        %>>>>>>> Geoffrey
+        %         sliderize(z, img, tmp2); % TODO make this work
     end
+    
+    % plot contour over this
+    hold on; contour(tmp2);
+    
+    % get new contour
+    tmp2 = editContour(f, tmp2, discretize);
+    if strcmp(tmp2, 'exit') % if exit
+        fprintf('layer %d was not saved\n', z);
+        return
+    end
+    
+    % stores it in 'mask' variable and saves it
+    mask(:,:,z) = tmp2;
+    save(F.tag('mask'), 'mask'); % overwrites the mask
+    fprintf('saved layer %d\n', z);
+    
+end
 
-    % pause and close current figure
-    pause(1); close gcf;
+% pause and close current figure
+pause(1); close gcf;
 
 end
 
@@ -111,33 +130,33 @@ end
 function newContour = editContour(f, oldContour, discretize)
 % getContour lets manually edit the contour
 
-    tmp2 = oldContour;
+tmp2 = oldContour;
 
-    [B, ~] = bwboundaries(tmp2,'noholes');
-    boundary = B{1};
-    boundary = boundary(1:discretize:end, :); % reduce the number of points by discretize
-    for k = 1:length(B)
-        plot(boundary(:,2), boundary(:,1), 'r', 'LineWidth', 2)
-    end
-    poly = impoly(gca, [boundary(:,2) boundary(:,1)]);
+[B, ~] = bwboundaries(tmp2,'noholes');
+boundary = B{1};
+boundary = boundary(1:discretize:end, :); % reduce the number of points by discretize
+for k = 1:length(B)
+    plot(boundary(:,2), boundary(:,1), 'r', 'LineWidth', 2)
+end
+poly = impoly(gca, [boundary(:,2) boundary(:,1)]);
 
 
+boundary_2 = poly.wait;
+if ~ishandle(f) % if figure was quit
+    fprintf('EXIT\n');
+    newContour = 'exit';
+    return
+end
+if isempty(boundary_2) % if poly was deleted
+    disp('draw your own poly');
+    poly = impoly;
     boundary_2 = poly.wait;
-    if ~ishandle(f) % if figure was quit
-       fprintf('EXIT\n');
-       newContour = 'exit';
-       return
-    end
-    if isempty(boundary_2) % if poly was deleted
-        disp('draw your own poly');
-        poly = impoly;
-        boundary_2 = poly.wait;
-    end
-    BW = poly2mask(boundary_2(:,1), boundary_2(:,2), size(tmp2,1), size(tmp2,2));
+end
+BW = poly2mask(boundary_2(:,1), boundary_2(:,2), size(tmp2,1), size(tmp2,2));
 
-    contour(BW)
+contour(BW)
 
-    newContour = BW;
+newContour = BW;
 
 end
 
@@ -156,12 +175,12 @@ function sliderize(z, img, tmp2)
         'Callback', @recomputesFilter);
     
     % show image
-    f.Visible = 'on'; 
+    f.Visible = 'on';
     hold on; contour(tmp2);
     
     % TODO find a way to stop execution since the value is not ok
     
-    function recomputesFilter(source, ~)
+function recomputesFilter(source, ~)
         bg_img = mean2(img);
         H = fspecial('disk',20);
         tmp1 = imfilter(img,H,'replicate');
