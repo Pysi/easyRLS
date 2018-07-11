b07ded51e63be8a5b0581e7d842505d4a12f31ce
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-07-11 19:01:52 +0200

    ReformatPhaseMap saves in addition a RGB stack

--- Matlab/CMTK/reformatPhasemap.m
+++ Matlab/CMTK/reformatPhasemap.m
@@ -1,4 +1,4 @@
-function reformatPhasemap(F,trans_mode)
+function reformatPhasemap(F,trans_mode,max)
 % use CMTK to reformat the imaginary and real part of phasemap
 
     % get path of reference brain
@@ -19,5 +19,35 @@ function reformatPhasemap(F,trans_mode)
         outPath = [outPathtag '.nrrd']; % TODO to tag
         CMTK_reformat(refPath, movPath, outPath, transPath); % perform reformating
     end
+    
+    cd(fullfile(regPath, refo)); % go to folder
+    file = dir('realpart.nrrd');
+    
+    [Ia,meta] = nrrdread(file.name);
+    Ia = double(Ia);
+    
+    file = dir('imaginary.nrrd');
+    Ib = double(nrrdread(file.name));
+    
+    % Save RGB images
+    outdir = fullfile(regPath, refo,'PhaseMap_rgb'); % directory
+    try
+        mkdir(outdir)
+    catch
+        'Directory exist already'
+    end
+    v_max = max;
+    clear imhsv
+    dim = sscanf(meta.sizes, '%d');
+   
+    for l = 1: dim(3)
+        l
+        imhsv(:,:,1) =   mod(atan2(Ib(:,:,l),Ia(:,:,l)) , 2*pi) / (2*pi);
+        imhsv(:,:,2) =   Ia(:,:,l,1)*0+1;
+        imhsv(:,:,3) =   sqrt( Ia(:,:,l).^2 + Ib(:,:,l).^2 )/v_max;
+        imwrite(hsv2rgb(imhsv),[outdir filesep 'layer' num2str(l,'%02d') '.tif']);
+    end
+
+
 
 end
