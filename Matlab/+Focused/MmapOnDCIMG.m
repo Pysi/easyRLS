28874e060c74bd8c53755a51199f1a558356d09c
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-10-25 20:30:11 +0200

    update

--- Matlab/+Focused/MmapOnDCIMG.m
+++ Matlab/+Focused/MmapOnDCIMG.m
@@ -5,12 +5,15 @@ function m = MmapOnDCIMG(F)
     % replace default values by exisiting if exist
     dcimgMatfile = [F.tag('dcimg') '.mat'];
     
-    try % if mat file already existing (ex: manually created)
+    % TODO uniformize the place where we get information !!!!!!!!!!!!
+    % pour éviter de prendre l'information à mille endroits du programme
+    
+    try % if mat file already existing (ex: manually created, created before)
         load(dcimgMatfile, 'x','y','z','t','Z','T','byteskip','clockskip','origSpace');
         
     catch % M % else, write it
         warning('dcimgMatfile not found, generating one');
-         
+        
         % define default values (will be replaced if more info is collected)
         byteskip = 808;
         clockskip = 8;
@@ -18,19 +21,28 @@ function m = MmapOnDCIMG(F)
         space = 'RAST';
         x = 614;
         y = 1018;
-        z = 20;
+        z = F.param.NLayers;
         t = F.param.NCycles;
+
+        try % tries to find a .toml file, if not, take default
+            file = dir(fullfile(F.dir('Images'), '*.toml'));
+            info = toml.read(fullfile(file.folder, file.name));
+            byteskip = info.dcimg.header;
+            clockskip = info.dcimg.clockskip;
+            fprintf('found byteskip=%d and clockskip=%d in toml file\n', byteskip, clockskip);
+        catch % keep default values 
+        end
 
 %         [~, ~, ord] = getTransformation(origSpace, space);
 
         % replace by focus values if available
         if isfield(F.IP, 'width') % if values available
 %             if ord(1) == 2 % if x and y inverted, TODO: more robust  
-%                 y = F.IP.height;
-%                 x = F.IP.width;
+                x = F.IP.height;
+                y = F.IP.width;
 %             else  
-                x = F.IP.width;
-                y = F.IP.height;            
+%                 x = F.IP.width;
+%                 y = F.IP.height;            
 %             end
         else
             warning('no tif file found, using default parameters (adjust them manually)');
