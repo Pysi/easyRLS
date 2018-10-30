3daebe117f85bac57ecbe552b2c8f86916244ee1
Hugo Trentesaux <hugo.trentesaux@u-psud.fr>
2018-10-30 15:42:55 +0100

    added margin to compute background
    to prevent baseline to be lower than background

--- Matlab/Compute/computeBackground.m
+++ Matlab/Compute/computeBackground.m
@@ -3,6 +3,9 @@ function computeBackground(F)
 % tag might be 'corrected'
 % RefIndex is the reference index used for drift correction
 
+F.Analysis.Background.MaxValue = 500; % upper limit for background
+F.Analysis.Background.Decrease = 10; % decrease the found background by this value to avoid being higher than baseline
+
     % create folder
     Focused.mkdir(F, 'Background');
 
@@ -19,7 +22,7 @@ function computeBackground(F)
     end
     
     % /!\ background verification
-    THRESHOLD = 500;
+    THRESHOLD = F.Analysis.Background.MaxValue;
     if max(background) > THRESHOLD % very unlikely value for background
         miniBackground = min(background);
         warning('unlikely high value for background :\n%s\nsetting values above %d to min (%f)\n', num2str(background), THRESHOLD, miniBackground);
@@ -30,6 +33,8 @@ function computeBackground(F)
         end
     end
 
+    % decrease background
+    background = background - F.Analysis.Background.Decrease;
     save(F.tag('background'), 'background');
 
 end
